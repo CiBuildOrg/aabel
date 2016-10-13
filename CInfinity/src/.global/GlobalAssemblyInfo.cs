82ce380d11bc2522ee1d597d2e3fb2c17ceea654
VLAD EMINOVICI <veminovici@hotmail.com>
2016-10-13 21:10:39 +0300

    Updated the inital versions.

--- CInfinity/src/.global/GlobalAssemblyInfo.cs
+++ CInfinity/src/.global/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("CInfinity")]
-[assembly: AssemblyProduct("Simplee")]
+[assembly: AssemblyProduct("CInfinity")]
 [assembly: AssemblyCopyright("Copyright (c) CInfinity 2015")]
 [assembly: AssemblyTrademark("CInfinity")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
