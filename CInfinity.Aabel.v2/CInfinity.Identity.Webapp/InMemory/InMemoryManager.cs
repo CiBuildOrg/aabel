7c5501447d5756beb542f92c97305888cc0dba97
VLAD EMINOVICI <veminovici@hotmail.com>
2016-11-24 11:35:09 +0200

    Added v2 of the Identity webapp for the CInfinity applications.

---
+++ CInfinity.Aabel.v2/CInfinity.Identity.Webapp/InMemory/InMemoryManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace CInfinity.Identity.Webapp
+{
+    public static class InMemoryManager
+    {
+        public static IEnumerable<Scope> GetScopes() =>
+            new Scope[]
+            {
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API 1"
+                }
+            };
+
+
+        public static IEnumerable<Client> GetClients() =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        "api1"
+                    }
+                }
+            };
+    }
+}
---
+++ CInfinity.Aabel.v2/CInfinity.Identity.Webapp/Properties/PublishProfiles/cinfinity-identity-webapp-dev - Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'c59d84e9-f2ff-476c-9f6f-783669e2d24f'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
